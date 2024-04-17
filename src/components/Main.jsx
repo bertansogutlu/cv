d79a14d00a1aeb6dcf127f4fa9f2a0dbf0dfeaa0
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-04-17 19:16:20 +0300

    Profile componenti olusturuldu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Hero from "../juniorComponents/Hero";
+import Hero from "../juniorComponents/Hero"
+import Skills from "../juniorComponents/Skills";
+import Profile from "../juniorComponents/Profile";
 
 function Main() {
   return (
     <main>
       <Hero />
+      <Skills/>
+      <Profile/>
     </main>
   );
 }
---
+++ src/juniorComponents/Profile.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const profile = {
+  topic: "Profile",
+  info: [
+    { title: "Dogum Tarihi", value: "05.02.1992" },
+    { title: "Ikamet Sehri", value: "Antalya" },
+    {
+      title: "Egitim Durumu",
+      value: "Yildiz Technical University, Mechanical Engineer",
+    },
+    { title: "Tercih Ettigi Rol", value: "Fronted" },
+  ],
+};
+
+const about = {
+  topic: "About Me",
+  text: "My engineering education fostered my analytical thinking skills and my ability to work with numbers, which I thought were indispensable for the sales position. My technical work experience, which you can see in the details of my resume, has provided me with the habit of working in harmony within teams from different cultures, taking responsibility of project management, capturing challenging deadlines under stress and doing result-oriented work both in Turkey and abroad.",
+};
+
+function Profile() {
+  return (
+    <article>
+      <h3 className="text-3xl font-semibold">Profile</h3>
+      <div className="flex">
+        <div className="w-3/8">
+          <h4 className="text-xl text-[#4338CA] py-3">Profile</h4>
+          <div>
+            {profile.info.map((item) => {
+              return <div className="flex">
+                <span className="w-1/3 font-semibold text-sm">{item.title}</span>
+                <p className="w-2/3 text-sm">{item.value}</p>
+              </div>;
+            })}
+          </div>
+        </div>
+        <div className="w-1/2 mx-auto text-xs">
+          <h4 className="text-xl text-[#4338CA] py-3">{about.topic}</h4>
+          <p>{about.text}</p>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default Profile;
